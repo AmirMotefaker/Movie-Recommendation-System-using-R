4974836c90a80f75ed3d541748236136a14daa28
Amir Motefaker <amotef@gmail.com>
2023-02-09 11:32:40 +0330

    Kaggle Notebook | Movie Recommendation System using R - BEST | Version 3

--- movie-recommendation-system-using-r-best.r
+++ movie-recommendation-system-using-r-best.r
@@ -2,15 +2,15 @@
  "cells": [
   {
    "cell_type": "markdown",
-   "id": "3a2497b2",
+   "id": "81590229",
    "metadata": {
     "_execution_state": "idle",
     "_uuid": "051d70d956493feee0c6d64651c6a088724dca2a",
     "papermill": {
-     "duration": 0.00214,
-     "end_time": "2023-02-09T07:55:52.172349",
+     "duration": 0.002213,
+     "end_time": "2023-02-09T08:02:30.015848",
      "exception": false,
-     "start_time": "2023-02-09T07:55:52.170209",
+     "start_time": "2023-02-09T08:02:30.013635",
      "status": "completed"
     },
     "tags": []
@@ -26,7 +26,21 @@
     "        - Collaborative filtering algorithms are divided into two categories:\n",
     "\n",
     "            - User-based collaborative filtering. The idea is to look for similar patterns in movie preferences in the target user and other users in the database.\n",
-    "            - Item-based collaborative filtering. The basic concept here is to look for similar items (movies) that target users rate or interact with.\n"
+    "            - Item-based collaborative filtering. The basic concept here is to look for similar items (movies) that target users rate or interact with.\n",
+    "\n",
+    "\n",
+    "# How to Build a Movie Recommendation System?\n",
+    "- The basics of film recommendation engines are in machine learning, we could on to building an actual movie recommendation system.\n",
+    "    1. Data. \n",
+    "        - ML systems need data, so find and import the essential libraries with movie datasets that already have global ratings.\n",
+    "    2. Analysis. \n",
+    "        - Create generic recommendations of top-rated movies from the existing dataset.\n",
+    "    3. Personalization. \n",
+    "        - Get personalized ratings by providing your own movie scores.\n",
+    "    4. Strategy. \n",
+    "        - Implement a content-based or collaborative filtering strategy.\n",
+    "    5. Combination. \n",
+    "        - Combine recommendation lists to get a reasonable estimate across the ratings. The combined dataset of movie ratings can now be used for either filtering model."
    ]
   }
  ],
@@ -46,14 +60,14 @@
   },
   "papermill": {
    "default_parameters": {},
-   "duration": 3.979479,
-   "end_time": "2023-02-09T07:55:52.294117",
+   "duration": 4.24631,
+   "end_time": "2023-02-09T08:02:30.140430",
    "environment_variables": {},
    "exception": null,
    "input_path": "__notebook__.ipynb",
    "output_path": "__notebook__.ipynb",
    "parameters": {},
-   "start_time": "2023-02-09T07:55:48.314638",
+   "start_time": "2023-02-09T08:02:25.894120",
    "version": "2.4.0"
   }
  },
