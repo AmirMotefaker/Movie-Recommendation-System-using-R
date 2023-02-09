2ea018f2d9818504ff3708b3ae75ec21fba3eaae
Amir Motefaker <amotef@gmail.com>
2023-02-09 11:37:34 +0330

    Kaggle Notebook | Movie Recommendation System using R - BEST | Version 5

--- movie-recommendation-system-using-r-best.r
+++ movie-recommendation-system-using-r-best.r
@@ -2,15 +2,15 @@
  "cells": [
   {
    "cell_type": "markdown",
-   "id": "05b5614d",
+   "id": "13d1513c",
    "metadata": {
     "_execution_state": "idle",
     "_uuid": "051d70d956493feee0c6d64651c6a088724dca2a",
     "papermill": {
-     "duration": 0.002273,
-     "end_time": "2023-02-09T08:05:22.754854",
+     "duration": 0.00176,
+     "end_time": "2023-02-09T08:07:26.453850",
      "exception": false,
-     "start_time": "2023-02-09T08:05:22.752581",
+     "start_time": "2023-02-09T08:07:26.452090",
      "status": "completed"
     },
     "tags": []
@@ -45,14 +45,14 @@
     "# Best Movie Datasets for Recommendation Systems in ML\n",
     "- [MovieLens 25M data set](https://grouplens.org/datasets/movielens/)\n",
     "- [IMDB Datasets](https://www.imdb.com/interfaces/)\n",
-    "- Linguistic Data of 32k Film Subtitles with IMBDb Meta-Data\n",
-    "- Film data set from UCI\n",
-    "- Full MovieLens dataset on Kaggle\n",
-    "- Cornell Film Review Data\n",
-    "- French National Cinema Center datasets\n",
-    "- Movie Industry on Kaggle\n",
-    "- Movie Body Counts\n",
-    "- Movie datasets on data.world"
+    "- [Linguistic Data of 32k Film Subtitles with IMBDb Meta-Data](https://data.world/robertjoellewis/film-subtitles)\n",
+    "- [Film data set from UCI](https://archive.ics.uci.edu/ml/datasets/Movie)\n",
+    "- [Full MovieLens dataset on Kaggle](https://www.kaggle.com/rounakbanik/the-movies-dataset)\n",
+    "- [Cornell Film Review Data](http://www.cs.cornell.edu/people/pabo/movie-review-data/)\n",
+    "- [French National Cinema Center datasets](https://www.data.gouv.fr/fr/organizations/centre-national-du-cinema-et-de-l-image-animee/)\n",
+    "- [Movie Industry on Kaggle](https://www.kaggle.com/danielgrijalvas/movies)\n",
+    "- [Movie Body Counts](http://moviebodycounts.com/)\n",
+    "- [Movie datasets on data.world](https://data.world/datasets/movies)"
    ]
   }
  ],
@@ -72,14 +72,14 @@
   },
   "papermill": {
    "default_parameters": {},
-   "duration": 3.494734,
-   "end_time": "2023-02-09T08:05:22.876625",
+   "duration": 3.416579,
+   "end_time": "2023-02-09T08:07:26.574870",
    "environment_variables": {},
    "exception": null,
    "input_path": "__notebook__.ipynb",
    "output_path": "__notebook__.ipynb",
    "parameters": {},
-   "start_time": "2023-02-09T08:05:19.381891",
+   "start_time": "2023-02-09T08:07:23.158291",
    "version": "2.4.0"
   }
  },
