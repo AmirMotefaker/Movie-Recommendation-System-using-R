98ca08fb5592fce3621ebd78d94894a546ccbb04
Amir Motefaker <amotef@gmail.com>
2023-02-09 11:26:02 +0330

    Kaggle Notebook | Movie Recommendation System using R - BEST | Version 2

--- movie-recommendation-system-using-r-best.r
+++ movie-recommendation-system-using-r-best.r
@@ -2,22 +2,31 @@
  "cells": [
   {
    "cell_type": "markdown",
-   "id": "62338ca2",
+   "id": "3a2497b2",
    "metadata": {
     "_execution_state": "idle",
     "_uuid": "051d70d956493feee0c6d64651c6a088724dca2a",
     "papermill": {
-     "duration": 0.00213,
-     "end_time": "2023-02-09T07:53:10.579982",
+     "duration": 0.00214,
+     "end_time": "2023-02-09T07:55:52.172349",
      "exception": false,
-     "start_time": "2023-02-09T07:53:10.577852",
+     "start_time": "2023-02-09T07:55:52.170209",
      "status": "completed"
     },
     "tags": []
    },
    "source": [
     "# What is Movie Recommendation?\n",
-    "- A movie recommendation system, or a movie recommender system, is an ML-based approach to filtering or predicting the users’ film preferences based on their past choices and behavior. It’s an advanced filtration mechanism that predicts the possible movie choices of the concerned user and their preferences towards a domain-specific item, aka movie.\n"
+    "- A movie recommendation system, or a movie recommender system, is an ML-based approach to filtering or predicting the users’ film preferences based on their past choices and behavior. It’s an advanced filtration mechanism that predicts the possible movie choices of the concerned user and their preferences towards a domain-specific item, aka movie.\n",
+    "\n",
+    "# Filtration Strategies for Movie Recommendation Systems\n",
+    "- The most popular categories of the ML algorithms used for movie recommendations include content-based filtering and collaborative filtering systems.\n",
+    "    - Content-Based Filtering\n",
+    "    - Collaborative Filtering\n",
+    "        - Collaborative filtering algorithms are divided into two categories:\n",
+    "\n",
+    "            - User-based collaborative filtering. The idea is to look for similar patterns in movie preferences in the target user and other users in the database.\n",
+    "            - Item-based collaborative filtering. The basic concept here is to look for similar items (movies) that target users rate or interact with.\n"
    ]
   }
  ],
@@ -37,14 +46,14 @@
   },
   "papermill": {
    "default_parameters": {},
-   "duration": 3.750817,
-   "end_time": "2023-02-09T07:53:10.702648",
+   "duration": 3.979479,
+   "end_time": "2023-02-09T07:55:52.294117",
    "environment_variables": {},
    "exception": null,
    "input_path": "__notebook__.ipynb",
    "output_path": "__notebook__.ipynb",
    "parameters": {},
-   "start_time": "2023-02-09T07:53:06.951831",
+   "start_time": "2023-02-09T07:55:48.314638",
    "version": "2.4.0"
   }
  },
