83625a1c3b65e863c383a78c9a931c4772b113f8
Amir Motefaker <amotef@gmail.com>
2023-02-09 11:23:19 +0330

    Kaggle Notebook | Movie Recommendation System using R - BEST | Version 1

---
+++ movie-recommendation-system-using-r-best.r
@@ -0,0 +1,53 @@
+{
+ "cells": [
+  {
+   "cell_type": "markdown",
+   "id": "62338ca2",
+   "metadata": {
+    "_execution_state": "idle",
+    "_uuid": "051d70d956493feee0c6d64651c6a088724dca2a",
+    "papermill": {
+     "duration": 0.00213,
+     "end_time": "2023-02-09T07:53:10.579982",
+     "exception": false,
+     "start_time": "2023-02-09T07:53:10.577852",
+     "status": "completed"
+    },
+    "tags": []
+   },
+   "source": [
+    "# What is Movie Recommendation?\n",
+    "- A movie recommendation system, or a movie recommender system, is an ML-based approach to filtering or predicting the users’ film preferences based on their past choices and behavior. It’s an advanced filtration mechanism that predicts the possible movie choices of the concerned user and their preferences towards a domain-specific item, aka movie.\n"
+   ]
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "R",
+   "language": "R",
+   "name": "ir"
+  },
+  "language_info": {
+   "codemirror_mode": "r",
+   "file_extension": ".r",
+   "mimetype": "text/x-r-source",
+   "name": "R",
+   "pygments_lexer": "r",
+   "version": "4.0.5"
+  },
+  "papermill": {
+   "default_parameters": {},
+   "duration": 3.750817,
+   "end_time": "2023-02-09T07:53:10.702648",
+   "environment_variables": {},
+   "exception": null,
+   "input_path": "__notebook__.ipynb",
+   "output_path": "__notebook__.ipynb",
+   "parameters": {},
+   "start_time": "2023-02-09T07:53:06.951831",
+   "version": "2.4.0"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+}
